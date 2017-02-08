5a27fd6184945747898b64b18967238f9d647492
Robert N. Steele <rnsteel1@asu.edu>
2017-02-07 16:35:07 -0800

    'Progress'

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,21 @@
 var express = require('express');
 var router = express.Router();
-
+var url = require('url');
+var mathGen = require('../lib/math');
 /* GET home page. */
 router.get('/', function(req, res, next) {
+  var username = url.parse(req.url, true);
+
   res.render('index', { title: 'MathAttack' });
+});
+router.post('/', function(req, res, next) {
+  res.redirect('login', { title: 'Login Successful' });
+});
+router.post('/login', function(req, res, next) {
+  var problem = mathGen.expressionGen();
+  res.render('problems', { title: 'Solve the problem below:',
+      firstTerm: problem.firstTerm, secondTerm: problem.secondTerm, operator: problem.operation, answer: problem.answer
+  });
 });
 
 module.exports = router;
